599920dafaf3fc5a53b195939e85616f44f9a437
Faiza Rao <159902088+faizarao2@users.noreply.github.com>
2024-12-09 00:42:18 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,20 @@
+// import Footer from "@/components/Footer";
+// import Header from "@/components/Header";
+import Hero from "/app/components/Hero";
+import Products from "/app/components/Products";
+import Slides from "@/app/components/Slides";
+import SubHero from "@/app/components/Subhero";
+
+export default function Home(){
+  return (
+    <>
+    {/* <Header/> */}
+    <Hero/>
+    <SubHero/>
+    <Products/>
+    <Slides/>
+    {/* <Footer/> */}
+    </>
+  )
+}
+      
